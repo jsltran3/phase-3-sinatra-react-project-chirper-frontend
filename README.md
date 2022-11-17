1a26ad43c023147764c19938ff96e816558eda79
Joseph <jsltran@gmail.com>
2022-11-17 15:25:33 -0800

    new app state

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # phase-3-sinatra-react-project-chirper-frontend
+
+# Description
+Welcome to my Phase 3 project: Chirper
---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Project 3!
+        </p>
+        <a>
+          Chirper
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
